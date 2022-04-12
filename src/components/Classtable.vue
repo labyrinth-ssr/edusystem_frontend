ee775b2eea14cdfeb9c3a05336dadb240d11e339
Shuran Sun <shuran_sun@163.com>
2022-04-12 11:03:19 +0800

    feat:后端session拦截，feat：修改节次多输入

--- src/components/Classtable.vue
+++ src/components/Classtable.vue
@@ -10,23 +10,41 @@
                 <el-dropdown-item v-for="index in classNum" :key="index" :command="index">{{index+1}}</el-dropdown-item>
             </el-dropdown-menu>
         </el-dropdown>
-        <el-time-select placeholder="起始时间" v-model="startTime" :picker-options="{
+        <!-- <el-time-select placeholder="起始时间" v-model="startTime[0]" :picker-options="{
       start: '08:30',
       step: '00:15',
       end: '22:00'
     }" @change="post_time">
         </el-time-select>
-        <el-time-select placeholder="结束时间" v-model="endTime" :picker-options="{
+        <el-time-select placeholder="起始时间" v-model="endTime[0]" :picker-options="{
       start: '08:30',
       step: '00:15',
       end: '22:00',
-      minTime: startTime
+      minTime: startTime[0]
     }" @change="post_time">
-        </el-time-select>
+        </el-time-select> -->
         <el-button type="primary" @click="set_classtime">
             修改上课时间
         </el-button>
             </el-form-item>
+        <el-form-item v-for="item in section" :key="item">
+                    <el-time-select placeholder="起始时间" v-model="startTime[item]" :picker-options="{
+      start: '08:30',
+      step: '00:15',
+      end: '22:00',
+      minTime: endTime[item-1]
+
+    }" @change="post_time">
+        </el-time-select>
+        <el-time-select placeholder="起始时间" v-model="endTime[item]" :picker-options="{
+      start: '08:30',
+      step: '00:15',
+      end: '22:00',
+      minTime: startTime[item]
+
+    }" @change="post_time">
+        </el-time-select>
+        </el-form-item>
             <el-form-item>
                 <el-dropdown >
             <span class="el-dropdown-link">
@@ -57,11 +75,12 @@ export default {
     name:'ClassroomTable',
     data() {
       return{
-        startTime: '',
-        endTime: '',
-        classNum:[/* ...(new Array(14)).keys() */],
+        startTime: [],
+        endTime: [],
+        classNum:[...(new Array(15)).keys()],
         classroomList:[/* {id:'H4101'},{id:'H4102'},{id:'H4103'} */],
         section:0,
+        display_section:0,
         classroom_add:'',
         tabledata:[]
       }
@@ -71,9 +90,9 @@ export default {
           
           this.classroomList=resp.data
       })
-      this.$axios.get('/classtime/admin/getsectionnum').then((resp)=>{
-          this.classNum=[...(new Array(resp.data)).keys()]
-      })
+    //   this.$axios.get('/classtime/admin/getsectionnum').then((resp)=>{
+    //       this.classNum=[...(new Array(resp.data)).keys()]
+    //   })
       this.$axios.get('/classtime/admin/getclasstime').then((resp)=>{
           this.tabledata=resp.data
       })
@@ -91,7 +110,8 @@ export default {
       },
       select_sec(command){
           this.section=command+1
-          console.log(this.section)
+          this.display_section=this.section-1
+          console.log(this.section-1)
       },
       add_classroom(){
           this.$axios.get('/classroom/admin/addclassroom/'+this.classroom_add).then((resp)=>{
@@ -102,14 +122,27 @@ export default {
           this.classroomList=resp.data
       })
       },
-      set_classtime(){
-if(this.startTime!=''&&this.endTime!=''&&this.section!=0){
-              console.log(this.startTime,this.endTime)
-            this.$axios.post('/classtime/admin/setclasstime',null,{params: {
-    'sectionInt': this.section,'time':this.startTime+'-'+this.endTime
-  }}).then((resp)=>{
-            console.log(resp.data)
-      })
+      set_classtime() {
+          if (this.startTime != '' && this.endTime != '' && this.section != 0) {
+              console.log(this.startTime, this.endTime)
+              var time=''
+              this.startTime.forEach((element, index) => { 
+                  if(index!=(this.startTime.length-1)){
+                  time+=element+'-'+this.endTime[index]+' '
+                  }
+                  else{
+                  time+=element+'-'+this.endTime[index]
+                  }
+               })
+               console.log(time)
+              this.$axios.post('/classtime/admin/setclasstime', null, {
+                  params: {
+                      'sectionInt': this.section,
+                      'time': time
+                  }
+              }).then((resp) => {
+                  console.log(resp.data)
+              })
           }
       }
     }
@@ -117,5 +150,7 @@ if(this.startTime!=''&&this.endTime!=''&&this.section!=0){
 </script>
 
 <style>
-
+.el-date-editor.el-input{
+    width:180px
+}
 </style>
