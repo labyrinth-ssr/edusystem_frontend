d2193f4fd9ad6273f8d6d46fd12b30a931f87d83
Shuran Sun <shuran_sun@163.com>
2022-04-12 11:33:04 +0800

    fix:登录

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,5 +1,7 @@
 <template>
 <el-container>
+      <change-passwd-dialog :visible="dialogVisible" @dialogclose='this.dialogVisible=false'/>
+
   <el-aside width="200px">
     <sidebar class="sidebar-container" />
   </el-aside>
@@ -17,14 +19,29 @@
 <script>
 import Sidebar from './Sidebar.vue'
 import Navbar from './NavBar.vue'
-
+import ChangePasswdDialog from '@/components/ChangePasswdDialog.vue';
 
 export default {
   name: 'Layout',
   components: {
     Navbar,
     Sidebar,
+    ChangePasswdDialog
   },
+      mounted(){
+        console.log(this.$store.state.first_login)
+      if(this.$store.state.first_login){
+        this.dialogVisible=true
+      }
+      else{
+        this.dialogVisible=false
+      }
+    },
+    data(){
+      return{
+        dialogVisible:''
+      }
+    }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,19 @@ import FileUpload from '@/components/FileUpload'
 
 Vue.use(VueRouter)
 
+const originalPush = VueRouter.prototype.push
+const originalReplace = VueRouter.prototype.replace
+// push
+VueRouter.prototype.push = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+// replace
+VueRouter.prototype.replace = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalReplace.call(this, location, onResolve, onReject)
+  return originalReplace.call(this, location).catch(err => err)
+}
+
 export const constantRouterMap = [
   {
     path: '/login',
