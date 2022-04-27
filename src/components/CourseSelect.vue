30e3e73e61d1e80addf70caa9c30933419710ce3
Shuran Sun <shuran_sun@163.com>
2022-04-27 18:30:41 +0800

    feat:选课筛选，模糊搜索，perf：选课页面样式

--- src/components/CourseSelect.vue
+++ src/components/CourseSelect.vue
@@ -1,116 +1,155 @@
 <template>
-<div>
-  <el-table border :data="selected_table" style="width: 100%" :cell-style="getCellStyle">
+  <div>
+    <el-table
+      border
+      :data="selected_table"
+      style="width: 100%"
+      :cell-style="getCellStyle"
+      :row-style="row_style"
+    >
       <el-table-column prop="sectionId" label="节次" width="80">
       </el-table-column>
-      <el-table-column v-for="index in day_num" :key="index" :prop="index+''" :label="'星期'+(index)">
-          <template slot-scope="scope">
-              {{scope.row[index+''].name}}
-          <!-- <el-row v-for="json in JSON.parse(scope.row.json)" :key="json">
-               {{json}}
-          </el-row> -->
-      </template>
+      <el-table-column
+        v-for="index in day_num"
+        :key="index"
+        :prop="index + ''"
+        :label="'星期' + index"
+      >
+        <template slot-scope="scope">
+          {{ scope.row[index + ""].name }}
+        </template>
       </el-table-column>
     </el-table>
-    <el-tabs type="border-card">
-  <el-tab-pane label="可选课程"><course-table-single :tableData="can_select_courses"/> </el-tab-pane>
-  <el-tab-pane  label="已选课程"><course-table-single @click_row="highlight_cell" :tableData="selected_courses"/> </el-tab-pane>
-  <el-tab-pane  label="已修课程"><course-table-single :tableData="learned_courses"/> </el-tab-pane>
-</el-tabs>
-        </div>
+    <el-tabs type="border-card" class="subpage">
+      <el-tab-pane label="可选课程"
+        ><course-table-single :tableData="can_select_courses" status="unselect"/>
+      </el-tab-pane>
+      <el-tab-pane label="已选课程"
+        ><course-table-single
+          @click_row="highlight_cell"
+          :tableData="selected_courses"
+          status="selected"
+        />
+      </el-tab-pane>
+      <el-tab-pane label="已修课程"
+        ><course-table-single :tableData="learned_courses" />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
-import CourseTableSingle from './CourseTableSingle.vue'
+import CourseTableSingle from "./CourseTableSingle.vue";
 export default {
   components: { CourseTableSingle },
-    name:'CourseSelect',
-    data(){
-        return{
-            day_num:7,
-            can_select_courses:[],
-            selected_courses:[],
-            learned_courses:[],
-            sememster:'2022.1',
-            stage:'1',
-            studentId:this.$store.state.user_id,
-            selected_table:[],
-            section_num:13,
-            highlight_pos:[]
-        }
+  name: "CourseSelect",
+  data() {
+    return {
+      day_num: 7,
+      can_select_courses: [],
+      selected_courses: [],
+      learned_courses: [],
+      sememster: "2022.1",
+      stage: "1",
+      studentId: this.$store.state.user_id,
+      selected_table: [],
+      section_num: 13,
+      highlight_pos: [],
+    };
+  },
+  methods: {
+    highlight_cell(posList) {
+      this.highlight_pos = posList;
     },
-    methods:{
-        highlight_cell(posList){
-            this.highlight_pos=posList
-        },
-        getCellStyle(cell){
-            // this.highlight_pos.forEach((pos)=>{
-                for (const pos of this.highlight_pos){
-if(((cell.rowIndex+1)+'')==pos.row&&(cell.columnIndex+'')==pos.col){
-                return 'background-color:antiquewhite';
-            }
-                }
-            
-            // })
-            
-        },
-        formattable(){
-            console.log(this.selected_courses)
+    row_style() {
+      return { height: "0px" };
+    },
+    getCellStyle(cell) {
+      if (this.highlight_pos.length != 0) {
+        for (const pos of this.highlight_pos) {
+          if (
+            (cell.rowIndex + 1 )+ "" == pos.row &&
+            cell.columnIndex + "" == pos.col
+          ) {
+            return { "background-color": "antiquewhite", padding: "0px" };
+          } 
+        }
+      }
+      return { padding: "0px" };
+      // return {padding:'0px'}
+    },
+    // })
 
-            for (let i=1;i<=this.section_num;i++){
-                this.selected_table.push({sectionId:i,'1':{},'2':{},'3':{},'4':{},'5':{},'6':{},'7':{}})
-            }
-            this.selected_courses.forEach((ele)=>{
-                console.log(ele)
-                var class_time=ele.class_time;
-                //1-3,1-4
-                class_time.split(',').forEach((period)=>{
-                    const split_res=period.split('-')
-                    // 1-3
-                    console.log(split_res)
-                    const section_id=parseInt(split_res[1]);
-                    this.selected_table[section_id-1][split_res[0]]={name:ele.name,id:ele.id};
-                })
-            })
-        }
+    formattable() {
+
+      for (let i = 1; i <= this.section_num; i++) {
+        this.selected_table.push({
+          sectionId: i,
+          1: {},
+          2: {},
+          3: {},
+          4: {},
+          5: {},
+          6: {},
+          7: {},
+        });
+      }
+      this.selected_courses.forEach((ele) => {
+        var class_time = ele.class_time;
+        //1-3,1-4
+        class_time.split(",").forEach((period) => {
+          const split_res = period.split("-");
+          // 1-3
+          const section_id = parseInt(split_res[1]);
+          this.selected_table[section_id - 1][split_res[0]] = {
+            name: ele.name,
+            id: ele.id,
+          };
+        });
+      });
     },
-    mounted(){
-        //TODO：先get permission
-        const selected_condition={
-                studentId:this.studentId,
-                sememster:this.sememster,
-                status:0
-            }
-        this.$axios.post('/course_sel/common/get_course/by_course_sel',selected_condition).then((resp)=>{
-            this.selected_courses=resp.data;
-            console.log(resp.data)
+  },
+  mounted() {
+    //TODO：先get permission
+    const selected_condition = {
+      studentId: this.studentId,
+      sememster: this.sememster,
+      status: 0,
+    };
+    this.$axios
+      .post("/course_sel/common/get_course/by_course_sel", selected_condition)
+      .then((resp) => {
+        this.selected_courses = resp.data;
         this.formattable();
-
-
-        })
-        const learned_condition={
-                studentId:this.studentId,
-                sememster:this.sememster,
-                status:2
-            }
-        this.$axios.post('/course_sel/common/get_course/by_course_sel',learned_condition).then((resp)=>{
-            this.learned_courses=resp.data;
-            console.log(resp.data)
-            
-
-        })
-        const can_select_condition={
-                studentId:this.studentId,
-            }
-        this.$axios.post('/course_sel/common/get_course/by_course_sel_complement',can_select_condition).then((resp)=>{
-            console.log(resp.data)
-            this.can_select_courses=resp.data;
-        })
-    }
-
-}
+      });
+    const learned_condition = {
+      studentId: this.studentId,
+      sememster: this.sememster,
+      status: 2,
+    };
+    this.$axios
+      .post("/course_sel/common/get_course/by_course_sel", learned_condition)
+      .then((resp) => {
+        this.learned_courses = resp.data;
+      });
+    const can_select_condition = {
+      studentId: this.studentId,
+    };
+    this.$axios
+      .post(
+        "/course_sel/common/get_course/by_course_sel_complement",
+        can_select_condition
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        this.can_select_courses = resp.data;
+      });
+  },
+};
 </script>
 
 <style>
-
+.subpage{
+    margin-top: 10px;
+}
 </style>
--- src/components/CourseTableSingle.vue
+++ src/components/CourseTableSingle.vue
@@ -1,10 +1,22 @@
 <template>
-   <el-table :data="tableData" height="572" style="width: 100%" @row-click="row_click" :row-style="row_style" highlight-current-row>
-            <el-table-column prop="id" label="课程代码" width="80">
+   <el-table :data="row_search()" style="width: 100%" @row-click="row_click" :row-style="row_style" :cell-style="{padding:'0px'}" highlight-current-row>
+            <el-table-column prop="id" label="课程代码" width="130">
+                <template slot="header" slot-scope="scope">
+            <el-input
+                v-model="course_id"
+                size="mini"
+                placeholder="课程代码"/>
+                </template>
             </el-table-column>
             <el-table-column prop="number" label="课程编号" width="80">
             </el-table-column>
-            <el-table-column prop="name" label="课程名" width="80">
+            <el-table-column prop="name" label="课程名" width="130">
+                <template slot="header" slot-scope="scope">
+            <el-input
+                v-model="course_name"
+                size="mini"
+                placeholder="课程名"/>
+                </template>
             </el-table-column>
             <el-table-column prop="department" label="开课院系" width="80">
             </el-table-column>
@@ -13,18 +25,27 @@
             <el-table-column prop="point" label="学分" width="80">
             </el-table-column>
             <el-table-column prop="teacher_id" label="任课教师id" width="100">
+                <template slot="header" slot-scope="scope">
+                <el-input
+                v-model="teacher"
+                size="mini"
+                placeholder="教师"/>
+                </template>
             </el-table-column>
-            <el-table-column prop="introduction" label="课程介绍" width="180">
+            <el-table-column prop="introduction" label="课程介绍">
             </el-table-column>
-            <el-table-column prop="class_time" label="上课时间" width="180">
+            <el-table-column prop="class_time" label="上课时间" :filters="classtime_list"
+      :filter-method="classtimeFilterHandler" width="180">
             </el-table-column>
-            <el-table-column prop="classroom_id" label="上课地点" width="80">
+            <el-table-column prop="classroom_id" label="上课地点" :filters="classroom_list"
+      :filter-method="classroomFilterHandler" width="100">
             </el-table-column>
             <el-table-column prop="max_student" label="任课容量" width="80">
             </el-table-column>
-            <el-table-column label="操作" width="170">
+            <el-table-column label="操作" >
                 <template >
-                    <el-button size="mini" >选课</el-button>
+                    <el-link :underline="true"  v-if="status=='unselect'" type="primary">选课</el-link>
+                    <el-link :underline="true"  v-if="status=='selected'" type="primary">退选</el-link>
                 </template>
             </el-table-column>
         </el-table>
@@ -36,15 +57,51 @@ export default {
     data(){
         return{
             highlight_rowid:'',
-            // status:'',
-            // tableData:[]
+            classroom_list:[],
+            classtime_list:[],
+            course_id:'',
+            course_name:'',
+            teacher:''
         }
     },
+    //什么时候
+    created(){
+        // .filter(data => !course_id || data.id.toLowerCase().includes(course_id.toLowerCase()))
+        //init filter list
+        var classroom_set=new Set();
+        this.tableData.forEach((ele)=>{
+            classroom_set.add(ele.classroom_id)
+        })
+            this.classroom_list=Array.from(classroom_set,(ele)=>{return {text:ele,value:ele}})
+        //课程时间filter：字符串匹配？无法使用级联...
+        //多选时间段后，只要部分匹配即可
+        var classtime_set=new Set();
+        this.tableData.forEach((ele)=>{
+            console.log(ele.class_time.split(','))
+            ele.class_time.split(',').forEach((time)=>{
+                classtime_set.add(time)
+            })
+        })
+            this.classtime_list=Array.from(classtime_set,(ele)=>{return {text:ele,value:ele}})
+    },
     methods:{
+        row_search(){
+            console.log(this.tableData)
+            return this.tableData.filter(data =>( !this.course_id || data.id.toLowerCase().includes(this.course_id.toLowerCase()) )&&
+            ( !this.course_name || data.name.toLowerCase().includes(this.course_name.toLowerCase()) )&&
+            ( !this.teacher || data.teacher_id.toLowerCase().includes(this.teacher.toLowerCase()) ))
+        },
+        classtimeFilterHandler(value, row, column) {
+        // const property = column['property'];
+        return row['class_time'].indexOf(value)!=-1;
+      },
+        classroomFilterHandler(value, row, column) {
+        // const property = column['property'];
+        
+        return row['classroom_id'] === value;
+      },
         row_style({row, rowIndex}){
-            if(rowIndex==this.highlight_rowid){
-                return "background-color:aliceblue";
-            }
+            return { height: "0px" };
         },
         row_click(row){
             console.log(row)
