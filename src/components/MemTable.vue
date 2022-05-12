ec74c7eb4857d027a8f9129f1c0a479dd89e0d36
yangfan909 <kexing2003@qq.com>
2022-05-12 23:30:45 +0800

    fix:修改merge错误

--- src/components/MemTable.vue
+++ src/components/MemTable.vue
@@ -1,31 +1,105 @@
-<!--WARNING:This is a test from zhx-->
+<script src="../router/index.js"></script>
 <template>
-  <inner-table :table="this.table" :handel1="this.handel1" :handel2="this.handel2">
+  <div>
+    <el-table
+        :data="table.data"
+        style="width: 100%"
+        :stripe = "true"
+        border
+        flex="left">
+      <template v-for="col in table.head">
+        <el-table-column :key="col.prop"
+                         :prop="col.prop"
+                         :label="col.label"
+                         :fixed="col.fix"
+                         :width="col.width">
+        </el-table-column>
+      </template>
+      <el-table-column
+          align="right"
+          :width=table.navWidth
+          fixed = "right">
+        <template slot="header" >
+          <el-menu :default-active="'/index'" router mode="horizontal" background-color="white" text-color="#222"
+                   active-text-color="red" style="min-width: 1300px">
+            <el-submenu>
+              <template slot="title"
+                        style="padding-left:10px">
+                <i class="el-icon-menu"></i>
+                <span slot="title">添加老师/学生</span>
+              </template>
+              <el-menu-item :key = "table.navList[0].path" :index="table.navList[0].path">
+                <template slot="title"
+                          style="padding-left:10px">
+                  <span slot="title">单个添加</span>
+                </template>
+              </el-menu-item>
+              <el-menu-item :key = "table.navList[1].path" :index="table.navList[1].path">
+                <template slot="title"
+                          style="padding-left:10px">
+                  <span slot="title">批量添加</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+            <el-menu-item :key = "table.navList.path" :index="table.navList.path">
+              <template slot="title"
+                        style="padding-left:10px">
+                <i class="el-icon-menu"></i>
+                <span slot="title">添加学院/专业</span>
+              </template>
+            </el-menu-item>
+          </el-menu>
+        </template>
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" size="middle">{{handel1.text}}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog title="修改学生/教师" :visible.sync="dialogVisible" :close-on-click-modal='false' v-if = "dialogVisible" :modal="false">
+      <div>
+        <user-form :action_prop="form_op" :formdata_prop="form"/>
+        <el-button  type="primary" @click="SubmitAdd">提交修改</el-button>
 
-  </inner-table>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 import InnerTable from "./innerTable";
-
+import UserForm from "./UserForm"
 export default {
-  name: "Notice_A_test",
-  components: {InnerTable},
+  name: "MemTable",
+  components: {InnerTable,UserForm},
   created() {
     this.clear()
   },
   data(){
-    function myEditable(rows) {
-      console.log(rows)
-      rows.setValue
-      return undefined;
-    }
-    function myHandleDelete(rows){
-      console.log(rows)
-      return undefined;
-    }
-
     return{
+      form_op:"add",
+      dialogVisible:false,
+      form:{
+        username: "",
+        role: "",
+        user_id: "",
+        id_number: "",
+        phone_number: "",
+        email: "",
+        major_department: [],
+        major: "",
+        department: ""
+      },
+      raw:{
+        username: "",
+        role: "",
+        user_id: "",
+        id_number: "",
+        phone_number: "",
+        email: "",
+        major_department: [],
+        major: "",
+        department: ""
+      },
       table:{
         'data': [
         ],
@@ -43,28 +117,26 @@ export default {
         ],
         navWidth:180,
         navList:[
-            {path: '/admin', name: '添加学生/教师',icon:"el-icon-circle-plus-outline"
-              ,'children':[
-                {
-                  path:'/admin/adduserform',
-                  icon:null,
-                  name:'单次添加'
-                },
-                {
-                  path: '/admin/upload',
-                  icon: null,
-                  name: '批量添加'
-                }
-              ]},
+          {
+            path:'/users/adduserform',
+            icon:null,
+            name:'单次添加'
+          },
+          {
+            path: '/users/addusercsv',
+            icon: null,
+            name: '批量添加'
+          }
         ]
       },
       handel1:{
         text:"编辑",
-        edit: myEditable
-        },
+        edit: this.myEditable
+      },
       handel2:{
-        text:"修改状态",
-        del:myHandleDelete
+        visible:false,
+        text:"",
+        del:this.myHandleDelete
       },
 
     }
@@ -76,6 +148,79 @@ export default {
             console.log(response.data)
             this.$data.table.data = response.data
           }).catch((failResponse)=>{console.log(failResponse)});
+    },
+    myEditable(rows) {
+      this.form_op='admin_add'
+      this.dialogVisible=true
+      this.form=JSON.parse(JSON.stringify(rows))
+      this.form.major_department =Array.from([rows.department, rows.major])
+      this.raw =  JSON.parse( JSON.stringify(rows) )
+      this.raw.major_department =Array.from([rows.department, rows.major])
+    },
+    myHandleDelete(rows){
+      let temp = rows;
+      temp.status ="quit";
+      console.log(rows)
+      this.$axios.put("/userinfo/admin/altuser",temp)
+          .then((response)=>{
+            console.log(response.data)
+            this.clear()
+          }).catch((err)=>{
+        console.log(err.data)
+      })
+
+      return undefined;
+    },
+    handleClick(row){
+      this.handel1.edit(row)
+    },
+    handleDelete(row){
+      this.handel2.del(row)
+    },
+    SubmitAdd() {
+      this.$axios.put("userinfo/admin/altuser", this.$data.form)
+          .then((response) => {
+            this.dialogVisible = false
+            console.log(response.data);
+            console.log(response.data['isOk'])
+            if (response.data['isOk'] === true) {
+
+              if (JSON.stringify(this.form) === JSON.stringify(this.raw)) {
+
+                this.$message(
+                    {
+                      type: "error",
+                      message: "尚未修改！"
+                    });
+              } else {
+
+                this.$message(
+                    {
+                      type: "success",
+                      message: "修改成功！"
+                    });
+                this.raw =  JSON.parse( JSON.stringify(this.form) )
+              }
+              this.clear();
+            } else {
+              this.$message(
+                  {
+                    type: "error",
+                    message: "修改失败！"
+                  })
+              this.form =  JSON.parse( JSON.stringify(this.raw) )
+            }
+          }).catch((error) => {
+        // this.raw =  JSON.parse( JSON.stringify(this.form) )
+        this.form =  JSON.parse( JSON.stringify(this.raw) )
+        this.$message(
+            {
+              type: "error",
+              message: "请求出错！"
+            })
+        console.log(error)
+
+      })
     }
   }
 
